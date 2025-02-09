679c1fe93fef18eaf13450c9532d7381ac6f898a
MarianSagat <marian.sagat@gmail.com>
2025-02-09 15:41:07 +0100

    start changes

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import easyocr
 from easyocr import Reader
-reader = easyocr.Reader(['en']) # this needs to run only once to load the model into memory
+reader = easyocr.Reader() # this needs to run only once to load the model into memory
 result = reader.readtext('D:/Documents/documents 2024/neural networks/code/EasyOCR2/examples/windowsPart.png')
 print(result)
